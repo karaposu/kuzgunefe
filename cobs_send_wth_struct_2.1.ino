ad3c5c83125a199dc7587252e6f68b3b17061a5a
Enes <31979644+karaposu@users.noreply.github.com>
2019-03-27 07:09:08 +0300

    Add files via upload

---
+++ cobs_send_wth_struct_2.1.ino
@@ -0,0 +1,138 @@
+/*This code takes values from structure and encode them with...
+ * Consistent Overhead Byte Stuffing (COBS) algoritm
+ * 
+ * First code checks the negative values. Save which values ...
+ * have a negative sign. After that converts each 16 bit values...
+ * to 8 bit and 8 bit values and stores them in array called
+ * "temp_array" . 
+ * 
+ * Since this process is in function we never .
+ * use that array name on the loop.We use return value directly 
+ * like this:
+ *  "make_prepackage(balance_robot)"  where 'balance_robot' is..
+ * the structure and 'make_prepackage' is the name of function.
+ * 
+ * After that we have a array which can be use as a input for 
+ * encoding function:
+ * StuffData()
+ * 
+ */
+int16_t adc=0;
+
+typedef struct  {
+    int16_t  angle;
+    int16_t output_raw;
+    int16_t output_pwm;
+    int16_t  p_value;
+    int8_t  i_value;
+    int8_t  d_value;
+}Balance_robot;
+
+unsigned char* make_prepackage( Balance_robot x);
+
+Balance_robot balance_robot;
+
+const int8_t length_of_package=13;
+void printArray(int *ptr, size_t length);     
+void StuffData(const unsigned char *ptr, unsigned long length, unsigned char *dst) ;
+unsigned char dest[length_of_package] ;
+char* target ;
+
+int xPin = A0; // A0-A5 analog pinlerinden herhangi birine bağlanabilir.
+int yPin = A1; // A0-A5 analog pinlerinden herhangi birine bağlanabilir.
+
+int dir_a=0;int dir_b=0;  int dir_c=0; int dir_d=0;
+ int dir_a2=0;int dir_b2=0;  int dir_c2=0; int dir_d2=0;
+
+
+
+
+
+void setup() {
+Serial.begin(115200);
+
+
+balance_robot.angle =35;
+balance_robot.output_raw =42;
+balance_robot.output_pwm =100;
+balance_robot.p_value =0;
+balance_robot.i_value =1;
+balance_robot.d_value =-16;
+
+}
+
+void loop() {
+//    int sensorValue = analogRead(A0);
+//    int sensorValue2 = analogRead(A1);
+//     int sensorValue3 = analogRead(A2);
+//   int sensorValue4 = analogRead(A3);
+
+int xPozisyonu = 0;
+int yPozisyonu = 0;
+
+  xPozisyonu = analogRead(xPin);
+  yPozisyonu = analogRead(yPin);
+   
+
+convert_from_physical( xPozisyonu, yPozisyonu , xPozisyonu, yPozisyonu )    ;   
+dir_a= dir_a*(1023/471);
+
+//Serial.print(joystick1_x); Serial.print(" ");
+//Serial.println(dir_a);
+
+balance_robot.angle =dir_a;
+balance_robot.output_raw =dir_a;
+balance_robot.output_pwm =dir_a;
+balance_robot.p_value =dir_a;
+balance_robot.i_value =1;
+balance_robot.d_value =1;
+ 
+    
+target=make_prepackage(balance_robot);//neccesary for allocation ?
+StuffData(target,(length_of_package-2),dest); 
+   for(int i =0 ; i<length_of_package; i++)
+  { Serial.write(dest[i]);}
+    delay(50);
+   }
+
+
+
+
+void convert_from_physical( int joystick1_x , int joystick1_y , int joystick2_x , int joystick2_y  )            
+{         
+ /* 1 joystickte 4 farklı yön var. 1. yön yukarı  2. yön aşagı 3.yön sag 4. yön sol olarak tanımlandı
+  * 1.yönün degerlerini ilk önce 0 -512 arasında değişen hale getirip sonra 2 ile çarpacagız.
+  * eger gelen deger 512den büyükse 512 çıkar.bu  benim dir_a degerimdir.
+  * 
+  */
+  
+      dir_a=0; dir_b=0;   dir_c=0;  dir_d=0;
+      dir_a2=0; dir_b2=0;   dir_c2=0;  dir_d2=0;
+
+     //joystick1_x=joystick1_x-40;
+
+  //   dir_a=  joystick1_x;
+     if (joystick1_x>552)      { dir_a=  joystick1_x-552;}
+//     else if(joystick1_x<553) {   dir_b=  joystick1_x;} 
+//
+//      if (joystick1_y>512)      { dir_c=  joystick1_y-512;}
+//     else if(joystick1_y<512) {   dir_d=  joystick1_y;} 
+//
+//     
+//     if (joystick2_x>512)      { dir_a2=  joystick2_x-512;}
+//     else if(joystick2_x<512) {   dir_b2=  joystick2_x;} 
+//
+//      if (joystick2_y>512)      { dir_c2=  joystick2_y-512;}
+//     else if(joystick2_y<512) {   dir_d2=  joystick2_y;} 
+} 
+
+
+
+
+
+
+
+
+
+
+
---
+++ functions.ino
@@ -0,0 +1,136 @@
+
+unsigned char* make_prepackage( Balance_robot x){
+  
+int8_t  lenght =9;
+
+int16_t temporary[6] ;
+int8_t  temp_array[lenght];
+int8_t  temp_array2[lenght]={0};
+int8_t n,n0,n1,n2,n3,n4,n5=0;
+
+//  Serial.write( x.angle ) ;// temporary[0]
+//  Serial.write( x.output_raw ) ;// temporary[0]
+//  Serial.write( x.output_pwm ) ;// temporary[0]
+//  Serial.write( x.p_value ) ;// temporary[0]
+//  Serial.write( x.i_value ) ;// temporary[0]
+//  Serial.write( x.d_value ) ;// temporary[0]
+
+     if(x.angle<0){  n0=1; temporary[0]=x.angle*-1;  }
+          else{  n0=0;temporary[0]=(x.angle);}
+        if(x.output_raw<0){ n1=1;  temporary[1]=(x.output_raw)*-1;}
+         else{ n1=0;temporary[1]=(x.output_raw);}
+                
+        if(x.output_pwm<0){ n2=1;    temporary[2]=(x.output_pwm)*-1;  }
+        else{ n2=0;  temporary[2]=(x.output_pwm);}
+                
+        if(x.p_value<0){ n3=1;  temporary[3]=(x.p_value)*-1;}
+        else{  n3=0; temporary[3]=(x.p_value);}
+                
+         if(x.i_value<0){ n4=1; temporary[4]=(x.i_value)*-1; }
+          else{ n4=0; temporary[4]=(x.i_value);}
+                
+         if(x.d_value<0){ n5=1; temporary[5]=(x.d_value)*-1;  }
+         else{ n5=0;temporary[5]=(x.d_value);}
+//
+//   for(int i =0 ; i<6; i++)
+//  { 
+//    Serial.write( temporary[i] ) ;
+//   }
+
+ //  int8_t n,n0,n1,n2,n3,n4,n5=0;
+n=n0*1+n1*2+n2*4+n3*8+n4*16+n5*32;
+//Serial.write(  n ) ;//x.angle
+
+
+//
+//PORTA = (TenBitVar & 0x00FF) // Lower byte
+//PORTB = (TenBitVar & 0xFF00) // Upper byte
+
+
+temp_array[0]= n;
+//
+//temp_array[1]= temporary[0] >> 8; // 256 ya böl
+//temp_array[2]= temporary[0]<< 2;  //  4 ile çarp
+
+
+temp_array[1]= (char)(temporary[0] & 0x00FF); // 35
+temp_array[2]= (char)(temporary[0]>> 8);  // 0
+
+temp_array[3]= (char)(temporary[1] & 0x00FF); // 42
+temp_array[4]= (char)(temporary[1]>> 8);  //  0
+
+
+temp_array[5]= (char)(temporary[2] & 0x00FF); // 100
+temp_array[6]= (char)(temporary[2]>> 8);  // 0
+
+temp_array[7]= (char)(temporary[3] & 0x00FF); // 0
+temp_array[8]= (char)(temporary[3]>> 8);  //  0
+
+temp_array[9]= temporary[4] ;   // 1
+temp_array[10]= temporary[5] ;   //32
+
+//
+// for(int i =0 ; i<11; i++)
+//  { 
+//   Serial.write( temp_array[i] ) ;//x.angle
+//   }
+  // Serial.write( 35 ) ;//x.angle
+
+// for(int i =0 ; i<9; i++)
+//  { 
+//   temp_array2[i]=  temp_array[i]  ;//x.angle
+//   // Serial.write( temp_array[3] ) ;
+//   }
+   
+delay(1);
+return  temp_array;
+ 
+  
+}
+
+
+
+#define FinishBlock(X) (*code_ptr = (X),  code_ptr = dst++,   code = 0x01  )
+void StuffData(const unsigned char *ptr, unsigned long length, unsigned char *dst) 
+{
+
+
+ unsigned char *zero=0;
+ unsigned char *dst2;
+ 
+  const unsigned char *end = ptr + length; //başlangıç adresi + uzunluk ile dizideki son degerin adresini belirle
+  unsigned char *code_ptr = dst++;  // dst nin 1. elemanının adresini code_ptr ye at
+  unsigned char code = 0x01; //counter for 255 adet data
+while (ptr < end) // ptr dizisinin sonuna ulaşana dek...
+{ 
+   if (*ptr == 0) FinishBlock(code); 
+  else { 
+    *dst++ = *ptr;
+    code++; 
+     if (code == 0xFF)FinishBlock(code);
+     } 
+    
+    ptr++; 
+   // if (ptr - end==0)  *dst++ = zero;
+  dst2  =dst;
+  } 
+   
+FinishBlock(code);
+dst=dst2;
+ //*dst++ = zero; 
+//  *dst++=*zero;
+}
+
+
+
+
+void printArray(char *ptr, size_t length)            
+{         
+    //for statement to print values using array             
+    size_t i = 0;
+    for( ; i < length; ++i )      
+    Serial.write(ptr[i]);        
+} 
+
+
+
